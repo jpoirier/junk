d8db85ffb1dc7c76466b940256502cd1fb21ab84
Dave Cheney <dave@cheney.net>
2013-09-12 15:36:39 +1000

    record clock skew

--- clock/examples/clockskew/main.go
+++ clock/examples/clockskew/main.go
@@ -9,10 +9,13 @@ import (
 func main() {
 	t := time.NewTicker(2 * time.Second)
 	rt, mt := clock.Realtime.Now(), clock.Monotonic.Now()
+	var rtt, mtt time.Duration
 	for {
 		<-t.C
 		rt1, mt1 := clock.Realtime.Now(), clock.Monotonic.Now()
-		fmt.Printf("CLOCK_REALTIME: %v\t CLOCK_MONOTONIC: %v\n", rt1.Sub(rt), mt1.Sub(mt))
+		rtt += rt1.Sub(rt)
+		mtt += mt1.Sub(mt)
 		rt, mt = rt1, mt1
+		fmt.Printf("CLOCK_REALTIME: %v\t CLOCK_MONOTONIC: %v\n", rtt, mtt)
 	}
 }
